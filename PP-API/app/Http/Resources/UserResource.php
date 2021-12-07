48a3bb3d45bf77db9d29b8274e3ebda0a2214a63
STEVE TENAS <stevetenadjang@gmail.com>
2021-12-07 07:23:07 +0100

    -  Creation and Completion of Request Rule for ProfilerID.

--- PP-API/app/Http/Resources/UserResource.php
+++ PP-API/app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'profiler_info' => profilerInfoResource::make($this->profilerInfos),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
---
+++ PP-API/app/Rules/ProfilerInfoIdRule.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\ProfilerInfo;
+use Illuminate\Contracts\Validation\Rule;
+
+class ProfilerInfoIdRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return ProfilerInfo::where('id', $value)->exists();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Unrecognized Profiler Id';
+    }
+}
