69ffd4473de764570af6d789884f11eb9e8b9cc6
STEVE TENAS <stevetenadjang@gmail.com>
2021-12-07 20:42:35 +0100

    - Creation of Seeders

--- PP-API/database/seeders/DatabaseSeeder.php
+++ PP-API/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProfilerAcademic;
+use App\Models\ProfilerContract;
+use App\Models\ProfilerEmail;
+use App\Models\ProfilerExp;
+use App\Models\ProfilerInfo;
+use App\Models\ProfilerIp;
+use App\Models\ProfilerLang;
+use App\Models\ProfilerMedical;
+use App\Models\ProfilerProject;
+use App\Models\ProfilerResident;
+use App\Models\ProfilerSkill;
+use App\Models\ProfilerTelephone;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,8 +24,20 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        ProfilerAcademic::factory(50)->create();
+        ProfilerContract::factory(100)->create();
+        ProfilerEmail::factory(50)->create();
+        ProfilerExp::factory(10000)->create();
+        ProfilerInfo::factory(1000)->create();
+        ProfilerIp::factory(500)->create();
+        ProfilerLang::factory(2)->create();
+        ProfilerMedical::factory(10)->create();
+        ProfilerProject::factory(1000)->create();
+        ProfilerResident::factory(10)->create();
+        ProfilerSkill::factory(2000)->create();
+        ProfilerTelephone::factory(10)->create();
     }
 }
